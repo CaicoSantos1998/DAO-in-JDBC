b51b08fdb42323dd21862915297ce93f4ea3fae1
CS <cs_1998@hotmail.com>
2026-01-13 21:35:20 -0300

    Department->All methods have been updated to try-with-resources

--- src/main/java/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -18,12 +18,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void insert(Department depart) {
-        PreparedStatement pst = null;
-        try {
-            pst = conn.prepareStatement(
-                    "INSERT INTO department " +
-                            "(Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS
-            );
+        try (PreparedStatement pst = conn.prepareStatement("INSERT INTO department (Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS)) {
             pst.setString(1, depart.getName());
             int rowsAffected = pst.executeUpdate();
             if(rowsAffected>0) {
@@ -38,38 +33,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             }
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        } finally {
-            DB.closeStatement(pst);
         }
     }
 
     @Override
     public void update(Department depart) {
-        PreparedStatement pst = null;
-        try {
-            pst = conn.prepareStatement(
-                    "UPDATE department " +
-                        "SET Name = ? " +
-                        "WHERE id = ?"
-            );
+        try (PreparedStatement pst = conn.prepareStatement("UPDATE department SET Name = ? WHERE id = ?")){
             pst.setString(1, depart.getName());
             pst.setInt(2, depart.getId());
             pst.executeUpdate();
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        } finally {
-            DB.closeStatement(pst);
         }
     }
 
     @Override
     public void deleteById(Integer id) {
-        PreparedStatement pst = null;
-        try {
-            pst = conn.prepareStatement(
-                    "DELETE FROM department " +
-                            "WHERE Id = ?"
-            );
+        try (PreparedStatement pst = conn.prepareStatement("DELETE FROM department WHERE Id = ?")) {
             pst.setInt(1, id);
             int rows = pst.executeUpdate();
             if(rows==0) {
@@ -77,22 +57,14 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             }
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        } finally {
-          DB.closeStatement(pst);
         }
     }
 
     @Override
     public Department findById(Integer id) {
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-        try {
-            pst = conn.prepareStatement(
-                    "SELECT * FROM department " +
-                            "WHERE id = ?"
-            );
+        try (PreparedStatement pst = conn.prepareStatement("SELECT * FROM department WHERE id = ?");
+        ResultSet rs = pst.executeQuery()) {
             pst.setInt(1, id);
-            rs = pst.executeQuery();
             if(rs.next()) {
                 Department dep = new Department();
                 dep.setId(rs.getInt("Id"));
@@ -102,22 +74,13 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             return null;
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        } finally {
-            DB.closeStatement(pst);
-            DB.closeResultSet(rs);
         }
     }
 
     @Override
     public List<Department> findAll() {
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-        try {
-            pst = conn.prepareStatement(
-                    "SELECT * FROM department " +
-                            "ORDER BY Name"
-            );
-            rs = pst.executeQuery();
+        try (PreparedStatement pst = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+        ResultSet rs = pst.executeQuery()) {
             List<Department> list = new ArrayList<>();
             while (rs.next()) {
                 Department dep = new Department();
@@ -128,9 +91,6 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             return list;
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        } finally {
-            DB.closeStatement(pst);
-            DB.closeResultSet(rs);
         }
     }
 }
